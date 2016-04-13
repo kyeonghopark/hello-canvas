7d928d64401506ac3692103c8b627924f666c4de
Kyeongho Park <kpark_ms@hotmail.com>
2016-04-13 03:19:50 +0000

    inital empty project files

---
+++ hello-canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('mainCanvas');
+var context = canvas.getContext('2d');
+
+context.rect(0, 0, canvas.width, canvas.height);
+context.fillStyle = "black";
+context.fill();
