5e01eeb1614c412acfd4e38e80b38acff0c9c301
Kyeong-Ho Park <kyeonghopark100@gmail.com>
2016-04-22 22:48:26 +0900

    add onmousedown and onkeydown event handlers.

--- client/app.js
+++ client/app.js
@@ -2,7 +2,26 @@
 
 
 var app = {
+  pos: {
+    x: canvas.width / 2,
+    y: canvas.height / 2
+  },
+  direction: 'none',
+  event: 'none',
   tickDisplay: ''
+};
+
+
+var moveApp = function() {
+  var posInc = {
+    none: {x: 0, y: 0},
+    left: { x: -1, y: 0 },
+    up: { x: 0, y: -1 },
+    right: { x: 1, y: 0 },
+    down: { x: 0, y: 1 }
+  }[app.direction];
+  app.pos.x += posInc.x;
+  app.pos.y += posInc.y;
 };
 
 
@@ -11,13 +30,38 @@ app.initialize = function() {
 
 
 app.onTick = function(tick) {
+  moveApp();
   app.tickDisplay = Math.floor(tick / 100).toString();
+};
+
+
+app.onMouse = function(canvasPos) {
+  app.event = 'mouse:' + JSON.stringify(canvasPos);
+
+  app.pos.x = canvasPos.x;
+  app.pos.y = canvasPos.y;
+};
+
+
+app.onKey = function(key) {
+  app.event = 'key:' + key.toString();
+
+  switch (key) {
+    case 37: app.direction = 'left'; break;
+    case 38: app.direction = 'up'; break;
+    case 39: app.direction = 'right'; break;
+    case 40: app.direction = 'down'; break;
+    default: app.direction = 'none'; break;
+  }
 };
 
 
 app.drawScreen = function() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'red';
+  context.fillText(app.event, app.pos.x, app.pos.y);
 
   context.fillStyle = 'blue';
   context.fillText(app.tickDisplay, 10, 10);
--- client/framework.js
+++ client/framework.js
@@ -4,6 +4,23 @@
 var nextTimeout = 0;  // millisec
 
 
+canvas.onmousedown = function(evt) {
+  var pos = {
+    x: evt.offsetX,
+    y: evt.offsetY
+  };
+  console.info({ mouse: pos });
+  app.onMouse(pos);
+};
+
+
+window.onkeydown = function(evt) {
+  var key = evt.keyCode;
+  console.info({ key: key });
+  app.onKey(key);
+};
+
+
 var initialize = function() {
   nextTimeout = 100;
   app.initialize();
