6025f05632fa2b81dac6aee7eba8f7c13330c201
Kyeong-Ho Park <kyeonghopark100@gmail.com>
2016-04-22 20:20:13 +0900

    display tick.

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 
-var app = {};
+var app = {
+  tickDisplay: ''
+};
 
 
 app.initialize = function() {
@@ -9,10 +11,14 @@ app.initialize = function() {
 
 
 app.onTick = function(tick) {
+  app.tickDisplay = Math.floor(tick / 100).toString();
 };
 
 
 app.drawScreen = function() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'blue';
+  context.fillText(app.tickDisplay, 10, 10);
 };
