298f6db8f2088159ff52049d9fa20ecb7b7ce5c8
Kyeong-Ho Park <kyeonghopark100@gmail.com>
2016-04-22 20:18:20 +0900

    implement framework tick.

--- client/app.js
+++ client/app.js
@@ -4,6 +4,14 @@
 var app = {};
 
 
+app.initialize = function() {
+};
+
+
+app.onTick = function(tick) {
+};
+
+
 app.drawScreen = function() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, canvas.width, canvas.height);
--- client/framework.js
+++ client/framework.js
@@ -3,6 +3,29 @@
 
 var canvas = document.getElementById('mainCanvas');
 var context = canvas.getContext('2d');
+var nextTimeout = 0;  // millisec
 
 
-app.drawScreen();
+var initialize = function() {
+  nextTimeout = 100;
+  app.initialize();
+};
+
+
+var startTick = function() {
+  var nextTick = 0;
+  window.requestAnimationFrame(function onAnimationFrame(tick) {
+    if (tick >= nextTick) {
+      app.onTick(tick);
+      app.drawScreen();
+      nextTick = tick + nextTimeout;
+    }
+    window.requestAnimationFrame(onAnimationFrame);
+  });
+};
+
+
+(function() {
+  initialize();
+  startTick();
+})();
